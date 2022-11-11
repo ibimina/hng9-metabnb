91a875a945a995185849610097a24ef75009305a
ibimina <96787210+ibimina@users.noreply.github.com>
2022-11-11 22:26:25 +0100

    docs: connect wallet modal component

--- src/components/connectwallet/ConnectWallet.jsx
+++ src/components/connectwallet/ConnectWallet.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
-
-export default function ConnectWallet() {
+import React from "react";
+import "./connectwallet.css";
+export default function ConnectWallet({ openW, handleOpenW }) {
   return (
-    <div>
+    <div className={`wallets_container ${!openW ? "open" : ""}`}>
       <div className="wallets">
         <div className="wallet_hd flex">
           <h3>Connect Wallet</h3>
-          <img src="/assets/x.svg" alt="criss-cross close icon" />
+          <img
+            src="/assets/x.svg"
+            alt="criss-cross close icon"
+            onClick={handleOpenW}
+          />
         </div>
+        <div className="choose">
+          <p className="margin">Choose your wallet</p>
+          <div className="flex space margin">
+            <div className="flex grey">
+              <img src="/assets/image 66.png" alt="Metamask icon" />
+              <p>Metamask</p>
+            </div>
+            <img src="/assets/Group (3).svg" alt="dropdown icon" />
+          </div>
 
-        <p>choose your wallet</p>
-        <div className="flex">
-          <img src="/assets/image 66.png" alt="" /> <p> Metamask</p>
-        </div>
-        <div className="flex">
-          <img src="/assets/image 69.png" alt="" />
-          <p>WalletConect</p>
+          <div className="flex space">
+            <div className="flex grey">
+              <img src="/assets/image 69.png" alt="Walletconnect icon" />
+              <p>WalletConect</p>
+            </div>
+            <img src="/assets/Group (3).svg" alt="dropdown icon" />
+          </div>
         </div>
       </div>
     </div>
