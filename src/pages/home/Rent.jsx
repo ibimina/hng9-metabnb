3cc996add44d23159734f3db6f8cbd3de6db08be
ibimina <96787210+ibimina@users.noreply.github.com>
2022-11-12 09:50:22 +0100

    docs: home page rent a place component

---
+++ src/pages/home/Rent.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function Rent() {
+  return (
+    <div className="home_container">
+      <div>
+        <h1>
+          Rent a <span className="purple">Place</span>away from{" "}
+          <span className="purple">Home</span>in the{" "}
+          <span className="purple">Metaverse</span>
+        </h1>
+        <p className="desc">
+          we provide you access to luxury and affordable house in the metaverse,
+          get a chance to turn your imagination to reality at your comfort zone
+        </p>
+        <form>
+          <input
+            type="text"
+            name=""
+            id=""
+            className="locate"
+            placeholder="Search for location"
+          />{" "}
+          <input type="submit" value="Search" className="search" />
+        </form>
+      </div>
+      <div className="location_imgcon">
+        <img src="/assets/Group 4028 (1).png" alt="" className="location_img" />
+      </div>
+    </div>
+  );
+}
