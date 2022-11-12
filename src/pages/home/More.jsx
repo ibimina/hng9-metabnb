b95205d78fa9a5ff524008464591c44c2735785a
ibimina <96787210+ibimina@users.noreply.github.com>
2022-11-12 20:09:02 +0100

    docs: Added jsx to More component

--- src/pages/home/More.jsx
+++ src/pages/home/More.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function More() {
   return (
-    <div>
-
-        
+    <div className="more_section">
+      <div className="more_content">
+        {" "}
+        <h4 className="wht_accent meta">Metabnb NFTs</h4>
+        <p className="wht_accent meta_con">
+          Discover our NFT gift cards collection. Loyal customers get amazing
+          gift cards which are traded as NFTs. These NFTs gives our customer
+          access to loads of our exclusive services.
+        </p>
+        <button className="more_btn">Learn more</button>
+      </div>
+      <img src="/assets/Frame 59546.png" alt="" className="more_image" />
     </div>
-  )
+  );
 }
